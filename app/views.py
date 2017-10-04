3a822d6834e535381c72664c51fa2564880b7480
mjcadz <mcadzow@TOPCON.COM>
2017-10-04 11:00:03 +1030

    cache control

--- app/views.py
+++ app/views.py
@@ -4,10 +4,18 @@ from flask import render_template
 
 from app import app
 
+from flask_cachecontrol import (
+    FlaskCacheControl,
+    cache)
+flask_cache_control = FlaskCacheControl()
+flask_cache_control.init_app(app)
+
 @app.route('/')
+@cache(max_age=604800, public=True)
 def index():
     return render_template("index.html")
 
 @app.route('/about')
+@cache(max_age=604800, public=True)
 def about():
     return render_template("about.html")
