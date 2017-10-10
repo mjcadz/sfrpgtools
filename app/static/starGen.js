394940affbbbee68fef57801bc328c19cfa3eae1
mjcadz <mcadzow@TOPCON.COM>
2017-10-10 11:16:11 +1030

    changes

--- app/static/starGen.js
+++ app/static/starGen.js
@@ -8,19 +8,19 @@ var dwarfStars = ["White Dwarf","Red Dwarf","Brown Dwarf","Black Dwarf"];
 var otherStars = ["Protostar","Neutron Star","Pulsar","Imminent Supernova"];
 var otherSystems = ["Black Hole","Ark/Generation Ship","Dyson Sphere","High Mass Debris Field","Deep Space Research Station","Starless Planet"];
 
-var starModifiers = ["Binary System","Nebula","Dust Rings","Asteroid Belt","Energy Harvesting Devices"];
+var starModifiers = ["Binary System","Nebula","Dust Rings","Asteroid Belt","Energy Harvesting Devices","Stellar Engine"];
 
 //PLANETS
 var terrestrialWorlds = ["Garden World","Rocky World","City World","Ocean World","Desert World","Forest World","Ice World","Lava World","Marsh World","Iron World","Coreless World","Carbon World"];
-var irregularWorlds =["Debris Field","Destroyed World","ProtoWorld","Ring World","Cube World","Refuelling Station","Offworld Trading Post"]
+var irregularWorlds =["Debris Field","Destroyed World","Protoplanet","Ring World","Cube World","Disk World","Shield World","Refuelling Station","Offworld Trading Post","Ship Scrapyard"]
 var gasGiants = ["Small Gas World","Gas Giant","Hot Gas Giant","Cold Gas Giant"];
 
-var worldModifiers = ["Rings","Super Rings","Terraformed","Terraform in Progress","Shielded","Force Field","Surrounded by Synthetic Debris","Large Axial Tilt","Eccentric Orbit","Retrograde Rotation"];
+var worldModifiers = ["Rings","Super Rings","Terraformed","Terraform in Progress","Force Field","Surrounded by Synthetic Debris","Large Axial Tilt","Perpendicular Rotation","Retrograde Rotation","Eccentric Orbit","Retrograde Orbit"];
 
 //SATELLITES
-var moons = ["Rocky Moon","Ice Moon","Forest Moon","Volcanic Moon","Marsh Moon","Garden Moon"];
-var irregularMoons = ["Captured Asteroid","Irregular Shaped Moon","Destroyed Moon","Ice Ball"];
-var artificialSatellites = ["Satellite Array","Space Station","Huge Space Station","Space Elevator","Planetary Defence Platform","Research Outpost","Galactic Navigation Beacon"];
+var moons = ["Rocky Moon","Ice Moon","Forest Moon","Volcanic Moon","Marsh Moon","Garden Moon","Gas Moon"];
+var irregularMoons = ["Captured Asteroid","Non-spherical Moon","Destroyed Moon","Ice Ball"];
+var artificialSatellites = ["Satellite Array","Space Station","Huge Space Station","Space Elevator","Planetary Defence Platform","Research Outpost","Galactic Navigation Beacon","Artificial Moon","Orbital Drydock","Orbital Mirrors"];
 
 //ATMOSPHERE
 var atmosphereType = ["Thin","Normal","Thick","None"];
@@ -33,29 +33,59 @@ var toxicGases = ["Ammonia","Methane","Carbon Monoxide","Carbon Dioxide","Hydroc
 //BIOMES
 var biomes = ["Aerial Terrian","Aquatic Terrian","Desert Terrain","Hill Terrian","Mountain Terrain","Marsh Terrain","Urban Terrain"]
 
+
+Number.prototype.between = function (min, max) {
+    return this > min && this < max;
+};
+
+
 function getStar() {
   var deck = [];
   var pick;
 
-  deck = deck.concat(mainStars);
-  deck = deck.concat(mainStars);
-  deck = deck.concat(giantStars);
-  deck = deck.concat(supergiantStars);
-  deck = deck.concat(dwarfStars);
-  deck = deck.concat(otherStars);
-  deck = deck.concat(otherSystems);
+  var num = getRandomInt(1, 100);
+  if (num.between(0,71)){
+    deck = deck.concat(mainStars);
+    deck = deck.concat(mainStars);
+    deck = deck.concat(giantStars);
+    deck = deck.concat(supergiantStars);
+  }
+  if (num.between(70,81)){
+    deck = deck.concat(dwarfStars);
+  }
+  if (num.between(80,91)){
+    deck = deck.concat(otherStars);
+  }
+  if (num.between(90,101)){
+    deck = deck.concat(otherSystems);
+  }
 
   pick = randomChoice(deck);
   return pick;
 }
 
-function getPlanet() {
+function getPlanet(position) {
   var deck = [];
   var pick;
 
-  deck = deck.concat(terrestrialWorlds);
-  deck = deck.concat(irregularWorlds);
-  deck = deck.concat(gasGiants);
+  var num = getRandomInt(1, 100);
+  if (num.between(0,51)){
+    deck = deck.concat(terrestrialWorlds);
+  }
+  if (num.between(50,81)){
+    deck = deck.concat(gasGiants);
+  }
+  if (num.between(80,101)){
+    deck = deck.concat(irregularWorlds);
+  }
+
+  if (position === "inner"){
+    deck = deck.concat(terrestrialWorlds);
+  }
+  else if (position === "outer"){
+    deck = deck.concat(gasGiants);
+    deck = deck.concat(gasGiants);
+  }
 
   pick = randomChoice(deck);
 
@@ -66,9 +96,16 @@ function getSatellite() {
   var deck = [];
   var pick;
 
-  deck = deck.concat(moons);
-  deck = deck.concat(irregularMoons);
-  deck = deck.concat(artificialSatellites);
+  var num = getRandomInt(1, 100);
+  if (num.between(0,41)){
+    deck = deck.concat(moons);
+  }
+  if (num.between(40,81)){
+    deck = deck.concat(artificialSatellites);
+  }
+  if (num.between(80,101)){
+    deck = deck.concat(irregularMoons);
+  }
 
   pick = randomChoice(deck);
   return pick;
@@ -78,9 +115,9 @@ function getSystem() {
   var system =[];
 
   system.push(getStar());
+  system.push(getPlanet("inner"));
   system.push(getPlanet());
-  system.push(getPlanet());
-  system.push(getPlanet());
+  system.push(getPlanet("outer"));
   system.push(getSatellite());
 
   return system
@@ -104,4 +141,12 @@ function printSystem() {
                       "<br>"+ system[4] + "</p>"
   );
 
+}
+
+/**
+* Returns a random integer between min (inclusive) and max (inclusive)
+* Using Math.round() will give you a non-uniform distribution!
+*/
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
