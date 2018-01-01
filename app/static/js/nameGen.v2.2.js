5a69be54704fea692dc171b3d046534ae00c3991
Sam Tyler <sam@samtyler.com.au>
2018-01-01 11:04:25 +1030

    Refactor name generator
    Separate data and logic to simplify main function.

--- app/static/js/nameGen.v2.2.js
+++ app/static/js/nameGen.v2.2.js
@@ -2,83 +2,253 @@ var adjective = ["A Gun Named","Absaloms","Accelerated","Adamant","Adamantium","
 
 var noun = ["Aegis","Air Strike","Alien","Artemis","Asteroid","Avenger","Barbarian","Bard","Bastard","Beast","Beestinger","Beholder","Bitch","Black Hole","Bouncer","Boy","Bulletstorm","Butcher","Cataclysm","Chaos","Claw","Cleric","Cloud","Comet","Compulsion","Constellation","Contingency","Crowd Control","D20","Dark Matter","Death","Defender","Delivery","Detective","Demigod","Diamond","Didact","Drama","Dragon","Dragons Breath","Druid","Duellist","Elite","Engine","Executioner","Fighter","Fist","Fox","Friendship","Galaxy","Gamma Ray","Ghost","Girl","Gladiator","Glitter","Goblin","Goddess","Godzilla","Heuristics","Hercules","Hornet","Infinity","Invention","Jaeger","Jealousy","Jewel","Joker","Justice","King","Kitten","Law","Lich","Madhouse","Magus","Massacre","Matter","Meteor","Monk","Moon","Nutrition","Netflix & Kill","Order","Owlbear","Pacifier","Paladin","Paragon","People's Elbow","Pepper","Photosphere","Pile Driver","Platform","Politician","Platypus","Protector","Pulsar","Seeker","Slave","Quarter-Pounder","Quartz","Quasar","Queen","Ranger","Rhino","Rogue","Ruby","Sapphire","Satellite","Savage","Shadow","Shine","Sorcerer","Star","Starburst","Steel","Stomper","Storm","Streetsweeper","Striker","Summoner","Supercluster","Talon","Tea & Biscuits","Threat","Thumper","Titan","Vacation","Vegan","Void","Whopper","Widow","Witch","Wizard","Zone"];
 
-var nounSlashing = ["Buzzblade","Diamond-Edged","Dimensional Slice","Drop Point","Microserrated","Molecular Rift","Serrated","Ultraserrated","Ultrathin","Zero-Edged"];
-
-var nounBludgeoning = ["Battle","Blunt","Combat","Gravity Well","Hardlight","Protector","Repeller","Sentinel","Skirmish","Warborn","Edgeless","Warfare"];
-
-var nounPiercing = ["Acceleration","Lightspeed","Mach I","Mach II","Mach III","Momentum","Speed-Force","Sub-light","Velocity","Warp-Speed","Hyper"];
-
-var nounAcid = ["Corroder","Decay","Scorpion","Dart-Frog","Green Lotus","Black Lotus","Black Dragon","Ooze","Vial","Heart Burn","Fume Extraction","Industrial Waste"];
-
-var nounSonic = ["Thunderstrike","Low Frequency Emitter","High Frequency Emitter","Banshee","Decibel","Sonic-Boom","Calamity","Hurricane","Howler","Cyclone","Thunderwave","Thunderclap"];
-
-var nounShock = ["Static ","Storm","Tempest","Lightning","Surge","Giga-Watt","Mega-Watt","Tumblespark","Direct Current","Flash","Bolt","Blue Dragon","Dynamo"];
-
-var nounFlame = ["Pyro","Incendiary","Inferno","Ember","Blaze","Salamander","Hellhound","Firedrake","Phoenix","Volcano","Cinder","Red Dragon","Magma"];
-
-var nounLaser = ["Azimuth","Corona","Aphelion","Perihelion","Parallax","Zenith","Nova","Prism","Spectrum","Aureole","Halo","Crystal","Optics","Rosette"];
-
-var nounCryo = ["Chill","Frostbite","Hailstorm","Blizzard","Avalanche","Frostdrake","Frost-Giant","Ice-Troll","Snowfall","Snowflake","White Dragon"];
-
-var nounPlasma = ["Solar Wind","Mass Ejection","Ion Cloud","Neutron Star","Star Scream","Blue Heat","White Heat","Solar Flare","Sun Storm","Ejecta","Ionosphere","Welder"];
-
 var manufacturer = ["AbadarCorp","Arabani Arms","Ulrikka","Idari","Eox Armory","Farraddi","Kalt & Ryder","Kosunagi Arsenal","LR Vostan","Stonewall","Weyland Industries","Hyperion","Aegis Solutions","Davetech","Vercite Traders","Vinson Dynamics","Kendachi","HyperChrome","Dyson Securities","Scrynet","Neon Photonics","G.L.A.I.V.E","Enforcer Inc.","Turbo Tactical","MantiCorps" ];
 
 var series = ["A3-","AAA-","ACR-","ACW-","AZU-","BAR-","CBR-","CQB-","CSP-","DX-","GR6-","GSG-","IXN-","KFS-","KHE-","KT-","LNR-","MACR-","NDT-","R9-","RHR-","RTA-","SBR-","STH-","TCR-","UTX-","Z5-","ZTN-","ZTR-"];
 
+var removedWords = [ "Acid", "Acid-edged", "Sonic", "Sonic-edged", "Shock", "Shock-edged", "Flame", "Flame-edged", "Cryo", "Cryo-edged", "Plasma", "Plasma-edged", "Kasathan", ];
+
+var terms = [
+  {
+    triggers: [
+      "Spear",
+      "Doshko",
+      "Pike",
+    ],
+    outputs: [
+      "Acceleration",
+      "Lightspeed",
+      "Mach I",
+      "Mach II",
+      "Mach III",
+      "Momentum",
+      "Speed-Force",
+      "Sub-light",
+      "Velocity",
+      "Warp-Speed",
+      "Hyper",
+    ]
+  },
+  {
+    triggers: [
+      "Knife",
+      "Sword",
+      "Longsword",
+      "Bladestaff",
+      "Handaxe",
+      "Greatsword",
+    ],
+    outputs: [
+      "Buzzblade",
+      "Diamond-Edged",
+      "Dimensional Slice",
+      "Drop Point",
+      "Microserrated",
+      "Molecular Rift",
+      "Serrated",
+      "Ultraserrated",
+      "Ultrathin",
+      "Zero-Edged",
+    ]
+  },
+  {
+    triggers: [
+      "Baton",
+      "Battleglove",
+      "Mace",
+      "Staff",
+      "Gauntlet",
+      "Hammer",
+      "Longhammer",
+      "Truncheon",
+    ],
+    outputs: [
+      "Battle",
+      "Blunt",
+      "Combat",
+      "Gravity Well",
+      "Hardlight",
+      "Protector",
+      "Repeller",
+      "Sentinel",
+      "Skirmish",
+      "Warborn",
+      "Edgeless",
+      "Warfare",
+    ]
+  },
+  {
+    triggers: [
+      "Acid",
+      "Acid-edged",
+    ],
+    outputs: [
+      "Corroder",
+      "Decay",
+      "Scorpion",
+      "Dart-Frog",
+      "Green Lotus",
+      "Black Lotus",
+      "Black Dragon",
+      "Ooze",
+      "Vial",
+      "Heart Burn",
+      "Fume Extraction",
+      "Industrial Waste",
+    ]
+  },
+  {
+    triggers: [
+      "Sonic",
+      "Sonic-edged",
+    ],
+    outputs: [
+      "Thunderstrike",
+      "Low Frequency Emitter",
+      "High Frequency Emitter",
+      "Banshee",
+      "Decibel",
+      "Sonic-Boom",
+      "Calamity",
+      "Hurricane",
+      "Howler",
+      "Cyclone",
+      "Thunderwave",
+      "Thunderclap",
+    ]
+  },
+  {
+    triggers: [
+      "Shock",
+      "Shock-edged",
+    ],
+    outputs: [
+      "Static",
+      "Storm",
+      "Tempest",
+      "Lightning",
+      "Surge",
+      "Giga-Watt",
+      "Mega-Watt",
+      "Tumblespark",
+      "Direct Current",
+      "Flash",
+      "Bolt",
+      "Blue Dragon",
+      "Dynamo",
+    ]
+  },
+  {
+    triggers: [
+      "Flame",
+      "Flame-edged",
+    ],
+    outputs: [
+      "Pyro",
+      "Incendiary",
+      "Inferno",
+      "Ember",
+      "Blaze",
+      "Salamander",
+      "Hellhound",
+      "Firedrake",
+      "Phoenix",
+      "Volcano",
+      "Cinder",
+      "Red Dragon",
+      "Magma",
+    ]
+  },
+  {
+    triggers: [
+      "Laser",
+    ],
+    outputs: [
+      "Azimuth",
+      "Corona",
+      "Aphelion",
+      "Perihelion",
+      "Parallax",
+      "Zenith",
+      "Nova",
+      "Prism",
+      "Spectrum",
+      "Aureole",
+      "Halo",
+      "Crystal",
+      "Optics",
+      "Rosette",
+    ]
+  },
+  {
+    triggers: [
+      "Cryo",
+      "Cryo-edged",
+    ],
+    outputs: [
+      "Chill",
+      "Frostbite",
+      "Hailstorm",
+      "Blizzard",
+      "Avalanche",
+      "Frostdrake",
+      "Frost-Giant",
+      "Ice-Troll",
+      "Snowfall",
+      "Snowflake",
+      "White Dragon",
+    ]
+  },
+  {
+    triggers: [
+      "Plasma",
+      "Plasma-edged",
+    ],
+    outputs: [
+      "Solar Wind",
+      "Mass Ejection",
+      "Ion Cloud",
+      "Neutron Star",
+      "Star Scream",
+      "Blue Heat",
+      "White Heat",
+      "Solar Flare",
+      "Sun Storm",
+      "Ejecta",
+      "Ionosphere",
+      "Welder",
+    ]
+  }
+];
+
+function replaceWord(word) {
+  var result = [];
+  terms.forEach(function(term) {
+    if (term.triggers.indexOf(word) > -1) {
+      result = term.outputs;
+    }
+  })
+  return result;
+}
+
 function getrandomName(weaponName){
   var name = [];
   var deck = [];
-  var concatAll = 0;
-  var stripName = weaponName;
 
   //stack the deck
   for (i = 0; i < 10; i++) {
     deck.push(randomChoice(noun));
   }
 
-  if (weaponName.includes("Knife") || weaponName.includes("Sword") || weaponName.includes("Longsword") || weaponName.includes("Bladestaff") || weaponName.includes("Handaxe") || weaponName.includes("Greatsword")){
-    deck = deck.concat(nounSlashing);
-  }
-  else if (weaponName.includes("Baton") || weaponName.includes("Battleglove") || weaponName.includes("Mace") || weaponName.includes("Staff") || weaponName.includes("Gauntlet") || weaponName.includes("Hammer") || weaponName.includes("Longhammer") || weaponName.includes("Truncheon")){
-    deck = deck.concat(nounBludgeoning);
-  }
-  else if (weaponName.includes("Spear") || weaponName.includes("Doshko") || weaponName.includes("Pike")){
-    deck = deck.concat(nounPiercing);
-  }else{
-    concatAll += 1;
-  }
+  weaponName.split(' ').forEach(function(word) {
+    deck = deck.concat(replaceWord(word));
+  })
 
-  if (weaponName.includes("Acid")){
-    deck = deck.concat(nounAcid);
-  }
-  else if (weaponName.includes("Sonic")){
-    deck = deck.concat(nounSonic);
-  }
-  else if (weaponName.includes("Shock")){
-    deck = deck.concat(nounShock);
-  }
-  else if (weaponName.includes("Flame")){
-    deck = deck.concat(nounFlame);
-  }
-  else if (weaponName.includes("Laser")){
-    deck = deck.concat(nounLaser);
-  }
-  else if (weaponName.includes("Cryo")){
-    deck = deck.concat(nounCryo);
-  }
-  else if (weaponName.includes("Plasma")){
-    deck = deck.concat(nounPlasma);
-  }
-  else {
-    concatAll += 1;
-  }
-
-  if (concatAll === 2){
-    deck = deck.concat(noun);
-  }
-
-  stripName = stripName.replace("Acid ", "").replace("Acid-edged ", "").replace("Sonic ", "").replace("Sonic-edged ", "").replace("Shock ", "").replace("Shock-edged ", "").replace("Flame ", "").replace("Flame-edged ", "").replace("Cryo ", "").replace("Cryo-edged ", "").replace("Plasma ", "").replace("Plasma-edged ", "").replace("Kasathan ", "");
+  //Remove unnecessary words
+  stripName = weaponName.split(' ').filter(function(word) {
+    console.log(word, removedWords.indexOf(word));
+    return removedWords.indexOf(word) === -1;
+  }).join(' ');
 
   name[0] = randomChoice(adjective) + " " + randomChoice(deck);
   name[1] = randomChoice(manufacturer);
