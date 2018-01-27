2512b752aeede401493f5ead875a997568868711
Sam Tyler <sam@samtyler.com.au>
2018-01-27 16:18:02 +1030

    Add starship tiers

--- app/static/js/shipData.js
+++ app/static/js/shipData.js
@@ -1,2 +1,145 @@
-//TODO: enter ship data
-console.log("Ship data is not implemented yet")
+var shipTiers = [
+  {
+    label: "1/4",
+    value: {
+      SBP: 25
+    }
+  },
+  {
+    label: "1/3",
+    value: {
+      SBP: 30
+    }
+  },
+  {
+    label: "1/2",
+    value: {
+      SBP: 40
+    }
+  },
+  {
+    label: "1",
+    value: {
+      SBP: 40
+    }
+  },
+  {
+    label: "2",
+    value: {
+      SBP: 75
+    }
+  },
+  {
+    label: "3",
+    value: {
+      SBP: 95
+    }
+  },
+  {
+    label: "4",
+    value: {
+      SBP: 115,
+      hpIncrease: 1
+    },
+  },
+  {
+    label: "5",
+    value: {
+      SBP: 135
+    }
+  },
+  {
+    label: "6",
+    value: {
+      SBP: 155
+    }
+  },
+  {
+    label: "7",
+    value: {
+      SBP: 180
+    }
+  },
+  {
+    label: "8",
+    value: {
+      SBP: 205,
+      hpIncrease: 2
+    }
+  },
+  {
+    label: "9",
+    value: {
+      SBP: 230
+    }
+  },
+  {
+    label: "10",
+    value: {
+      SBP: 270
+    }
+  },
+  {
+    label: "11",
+    value: {
+      SBP: 310
+    }
+  },
+  {
+    label: "12",
+    value: {
+      SBP: 350,
+      hpIncrease: 3
+    }
+  },
+  {
+    label: "13",
+    value: {
+      SBP: 400
+    }
+  },
+  {
+    label: "14",
+    value: {
+      SBP: 450
+    }
+  },
+  {
+    label: "15",
+    value: {
+      SBP: 500
+    }
+  },
+  {
+    label: "16",
+    value: {
+      SBP: 600,
+      hpIncrease: 4
+    }
+  },
+  {
+    label: "17",
+    value: {
+      SBP: 700
+    }
+  },
+  {
+    label: "18",
+    value: {
+      SBP: 800
+    }
+  },
+  {
+    label: "19",
+    value: {
+      SBP: 900
+    }
+  },
+  {
+    label: "20",
+    value: {
+      SBP: 1000,
+      hpIncrease: 5
+    }
+  }
+]
