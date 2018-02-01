7f57d4d57f8d5b247f6a0b4b6b5f6d920dffe598
mjcadz <mjcadzdev@gmail.com>
2018-02-01 00:56:52 +0000

    force ssl

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, url_for
+from flask_sslify import SSLify
 
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
+sslify = SSLify(app)
 
 # Load the views
 from app import views
