1179a7e96d155630ce51827e4ac3cd9f13bbfc93
Unknown <mjcadzdev@gmail.com>
2018-03-15 22:46:51 +1030

    data template

--- app/static/js/encounterGen.v2.0.js
+++ app/static/js/encounterGen.v2.0.js
@@ -12,6 +12,18 @@ function hideAdvanced() {
   $('#advanced').collapse('hide')
 }
 
+function showMonsterPicker() {
+  $('#pickerButton').text('Hide monster picker');
+  $('#monsterPicker').collapse('show');
+  $('#pickerButton').attr('onclick', 'hideMonsterPicker()')
+}
+
+function hideMonsterPicker() {
+  $('#pickerButton').text('Show monster picker');
+  $('#monsterPicker').collapse('hide');
+  $('#pickerButton').attr('onclick', 'showMonsterPicker()');
+}
+
 //runs when page is loaded
 $( document ).ready(function() {
   //initialise pickers
---
+++ app/static/js/monsterData.v2.0.js
@@ -0,0 +1,32 @@
+var equipmentData = {
+  /*"Name":{
+    "cr": "val",
+    "combatType": "val",
+    "alignment": "val",
+    "size": "val",
+    "type": "val",
+    "subtype": "val",
+    "environment", "val",
+    "temperature", "val",
+    "planet", "val",
+    "organization", "val",
+    "template", "val",
+    "source", "val",
+    "page", "val"
+  },*/
+  "Name":{
+    "cr": "val",
+    "combatType": "val",
+    "alignment": "val",
+    "size": "val",
+    "type": "val",
+    "subtype": "val",
+    "environment", "val",
+    "temperature", "val",
+    "planet", "val",
+    "organization", "val",
+    "template", "val",
+    "source", "val",
+    "page", "val"
+  },
+};
