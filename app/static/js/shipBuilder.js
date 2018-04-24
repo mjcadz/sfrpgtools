a84470a9adb41dfaec2d91f2047678c8c1902f11
Unknown <mjcadzdev@gmail.com>
2018-04-24 16:52:43 +0930

    Ship gen progress

--- app/static/js/shipBuilder.js
+++ app/static/js/shipBuilder.js
@@ -39,8 +39,100 @@ function generateDropdown(parentID,label,dropID,title,array) {
   $('#'+dropID).on('changed.bs.select', dropClickHandler);
 }
 
+function generateShip() {
+  $('.btn-edit').show();
+  $('.btn-image').show();
+  $('.btn-print').show();
+  displayShipBlock()
+
+}
+
+function displayShipBlock() {
+    //
+    //Ship Block
+    //
+
+    textBlock = "";
+    //description
+    textBlock += '<hr>';
+    textBlock += leftAndRight('<b>' + "Name" + '</b>','<b>TIER '+ "5" +'</b>');
+    textBlock += '<hr>';
+    textBlock += "<div>" + "Medium transport" + "</div>";
+    textBlock += "<div>" + "<b>Speed</b> 8; " + "<b>Maneuverability</b> average (2 turn); " + "<b>Drift</b> 2" + "</div>";
+    textBlock += "<div>" + "<b>AC</b> 22; " + "<b>TL</b> 22" + "</div>";
+    textBlock += "<div>" + "<b>Shields</b> X; " + "</div>";
+    textBlock += "<div>" + "<b>Attack (Forward)</b> X; " + "</div>";
+    textBlock += "<div>" + "<b>Systems</b> X; " + "</div>";
+    textBlock += "<div>" + "<b>Modifiers</b> X; " + "</div>";
+    textBlock += "<div><b>CREW</b></div>";
+    textBlock += '<hr>';
+    textBlock += "<div>" + "<b>Captain</b> X; " + "</div>";
+
+    //statBlock.CreatureType
+    var $StatBlock = $(".summernoteEdit").first();
+    $StatBlock.empty();
+    $StatBlock.append(textBlock);
+}
+
+//returns a string div with right aligned and left alignedtext on the same line
+function leftAndRight(left,right){
+  return '<div class="row"><div class="col-xs-8"><div>' + left + '</div></div><div class="col-xs-4"><div class="text-right">' + right + '</div></div></div>'
+}
+
+//show the summernote edit box wrapped around the statblock text
+function editBlock() {
+  $('.btn-save').show();
+  $('.btn-edit').hide();
+  $('.btn-image').hide();
+  $('.btn-print').hide();
+  $('#overEdit').css({'margin-left': '0em', 'margin-right': '0em'});
+  $('.summernoteEdit').summernote({
+    focus: true,
+    toolbar: [
+      // [groupName, [list of button]]
+      ['style', ['bold', 'italic', 'underline', 'superscript', 'subscript', 'clear', 'color']],
+      ['fontsize', ['fontsize']],
+      ['insert', ['link','hr','picture']],
+      ['misc', ['fullscreen','codeview',]],
+    ],
+    print: {
+        'stylesheetUrl': 'static/css/print.css'
+    }
+  });
+};
+
+//remove the edit box and show straight html
+function saveBlock() {
+  $('.btn-save').hide();
+  $('.btn-edit').show();
+  $('.btn-image').show();
+  $('.btn-print').show();
+  $('#overEdit').css({'margin-left': '4.5em', 'margin-right': '1em'});
+  var markup = $('.summernoteEdit').summernote('code');
+  $('.summernoteEdit').summernote('destroy');
+};
+
+//generate image from statblock so user can save
+function blockToImage() {
+  html2canvas(document.querySelector("#capture")).then(canvas => {
+
+    canvas.toBlob(function(blob) {
+    	saveAs(blob, "statblock.png");
+    });
+  });
+};
+
+//print the statblock
+function printBlock() {
+    window.print();
+};
+
 //runs when page is loaded
 $( document ).ready(function() {
   //initialise pickers
   $('.selectpicker').selectpicker();
+  $('.btn-save').hide();
+  $('.btn-edit').hide();
+  $('.btn-image').hide();
+  $('.btn-print').hide();
 });
