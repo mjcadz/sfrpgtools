07865eecafb567514670b0e16b584fa44fab6090
mjcadz <mjcadzdev@gmail.com>
2018-05-14 12:06:30 +0000

    remove sslify afterserver migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask, url_for
-from flask_sslify import SSLify
-
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
-sslify = SSLify(app)
 
 # Load the views
 from app import views
