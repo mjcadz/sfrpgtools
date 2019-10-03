59c8ec6ecbcedd26fb5fc07e263be474f8900658
mjcadz <mjcadzdev@gmail.com>
2019-10-03 16:12:27 +0930

    Auto stash before rebase of "master"

--- app/static/js/armorGen.v2.1.js
+++ app/static/js/armorGen.v2.1.js
@@ -126,6 +126,7 @@ var armorNameHeavy2 = [
   "Thinplate"
 ]
 
+// flavourtext
 var flavorTextLight = [
   "Is made from a high tech material that looks and feels like a t-shirt",
   "Has light plating hidden underneath the environmental suit",
